04ce7e92588f4917b179c5400b794f4f5657d94d
Kevin Gorham <kevin.gorham@electriccoin.co>
2021-08-22 23:42:22 -0400

    Fix: Compile errors related to recent changes in the Twig APIs.

--- app/src/main/java/cash/z/ecc/android/ui/util/DebugFileTwig.kt
+++ app/src/main/java/cash/z/ecc/android/ui/util/DebugFileTwig.kt
@@ -2,15 +2,14 @@ package cash.z.ecc.android.ui.util
 
 import cash.z.ecc.android.ZcashWalletApp
 import cash.z.ecc.android.sdk.ext.TroubleshootingTwig
-import cash.z.ecc.android.sdk.ext.spiffy
 import okio.Okio
 import java.io.File
 
 class DebugFileTwig(fileName: String = "developer_log.txt") : TroubleshootingTwig(formatter = spiffy(6)) {
     val file = File("${ZcashWalletApp.instance.filesDir}/logs", fileName)
 
-    override fun twig(logMessage: String) {
-        super.twig(logMessage)
+    override fun twig(logMessage: String, priority: Int) {
+        super.twig(logMessage, priority)
         appendToFile(formatter(logMessage))
     }
 
